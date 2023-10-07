790c48abab04306093dedffeae9d8bc86a32f2c3
Douglas Lowder <douglowder@mac.com>
2023-10-07 13:52:47 -0700

    TV fix for Time.h

--- folly/portability/Time.h
+++ folly/portability/Time.h
@@ -31,7 +31,8 @@
         ((!defined(TARGET_OS_OSX) || TARGET_OS_OSX) &&                 \
          (MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_12)) || \
     (defined(TARGET_OS_IPHONE) && TARGET_OS_IPHONE != 0 &&             \
-     (__IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_10_0))
+     (__IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_10_0)) ||           \
+    (defined(TARGET_OS_TV) && TARGET_OS_TV != 0)
 
 #ifdef FOLLY_HAVE_CLOCK_GETTIME
 #undef FOLLY_HAVE_CLOCK_GETTIME
